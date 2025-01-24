7ea2ed8c2670214911743182900a30358155906a
Pulquerio <jonas.cross@hotmail.com>
2025-01-24 17:52:03 -0300

    correções no css Background e InfoMemories

--- src/components/InfoMemories/index.jsx
+++ src/components/InfoMemories/index.jsx
@@ -10,13 +10,13 @@ function InfoMemories() {
     <section className={styles.conteudo}>
       <div className={styles.descricao}>
         <p className={styles.datas}>13 de Abril, 2022</p>
-        <img src={RapazConcentrado} alt="" />
-        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique...</p>
-        <p>Ler mais <FontAwesomeIcon style={{ color: "red" }} icon={faArrowRight} /></p>
+        <img className={styles.img_rapaz_infoMemories} src={RapazConcentrado} alt="" />
+        <p className={styles.pText}>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique...</p>
+        <p>Ler mais <FontAwesomeIcon icon={faArrowRight} /></p>
       </div>
       <div className={styles.descricao}>
         <p className={styles.datas}>20 de Maio, 2022</p>
-        <img src={RapazDiscursando} alt="" />
+        <img className={styles.img_rapaz_infoMemories} src={RapazDiscursando} alt="" />
         <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique...</p>
       </div>
     </section>
